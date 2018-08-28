d4ca2ff4eccd07aa21c026915ae2b55bd2f0f666
vasile2k <andy.dorcu@yahoo.com>
2018-08-28 20:21:26 +0000

    Small fix

--- test/test.js
+++ test/test.js
@@ -2,25 +2,34 @@ var assert = require("assert");
 describe("vas-chat-reactor", function(){
     describe("Load plugin", function(){
         
-        var EventEmitter = undefined;
-        var bot = undefined;
-        
         it("should load events", function(){
+            
+            var EventEmitter = undefined;
+            var bot = undefined;
+            
             EventEmitter = require("events");
             assert(EventEmitter != undefined);
             bot = new EventEmitter();
             assert(bot != undefined);
+            
         })
         
         it("should add functions to the bot", function(){
-            require("./index.js")(bot);
+            
+            var bot = new (require("events"))();
+            require("../index.js")(bot);
             assert(bot);
             assert(bot.enableChatReaction);
             assert(bot.disableChatReaction);
+            
         });
         
         it("should not crash on message event", function(){
+            
+            var bot = new (require("events"))();
+            require("../index.js")(bot);
             bot.emit("message", "test");
+            
         });
         
     });
